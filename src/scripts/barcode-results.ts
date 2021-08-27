11af31de5042c821229406cc57b24eab1d344161
Justin Noel <justin.noel@calendee.com>
2021-08-27 16:47:19 -0500

    feat: add styling

--- src/scripts/barcode-results.ts
+++ src/scripts/barcode-results.ts
@@ -1,13 +1,29 @@
-function createProductRow(resultsBody, productInfo) {
+function createProductRow(resultsBody, productInfo, counter) {
 	const { barcode, status, info } = productInfo;
 	const { title, brand, manufacturer, weight } = info;
-	const newRow = resultsBody.insertRow(0);
+	const oddEven = counter % 2 === 0 ? "odd" : "even";
 	const rowData = [barcode, title, brand, manufacturer, weight, status];
 
-	rowData.forEach((cellData) => {
+	const newRow = resultsBody.insertRow(0);
+	newRow.classList.add(oddEven === "odd" ? "bg-white" : "bg-gray-50");
+
+	rowData.forEach((cellData, index) => {
 		const newCell = newRow.insertCell();
+		newCell.classList.add(
+			"px-6",
+			"py-4",
+			"whitespace-nowrap",
+			"text-sm",
+			"font-medium",
+			index === 0 ? "text-gray-700" : "text-gray-500",
+		);
+
+		const cellDataSpan = document.createElement("span");
+		cellDataSpan.classList.add("inline-block", "max-ch");
+
 		const cellText = document.createTextNode(cellData);
-		newCell.appendChild(cellText);
+		cellDataSpan.appendChild(cellText);
+		newCell.appendChild(cellDataSpan);
 	});
 }
 
@@ -20,12 +36,12 @@ export function processBarcodeResults() {
 	const resultsBody = document.getElementById("results-body");
 	let counter = 0;
 	function handleScanEvent(event) {
-		createProductRow(resultsBody, event.detail);
+		createProductRow(resultsBody, event.detail, counter);
 
 		if (counter === 0) {
 			unhideResults();
-			counter = counter + 1;
 		}
+		counter = counter + 1;
 	}
 
 	document.addEventListener("scan-completed", handleScanEvent);
