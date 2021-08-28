8e942a90dcff2ecac8780987d4245e9620c23d0d
Justin Noel <justin.noel@calendee.com>
2021-08-28 06:05:26 -0500

    refactor: move functions to utility file

--- src/scripts/barcode-results.ts
+++ src/scripts/barcode-results.ts
@@ -1,39 +1,4 @@
-function createProductRow(resultsBody, productInfo, counter) {
-	const { barcode, status, info } = productInfo;
-	const { title, brand, manufacturer, weight } = info;
-	const oddEven = counter % 2 === 0 ? "odd" : "even";
-	const rowData = [barcode, title, brand, manufacturer, weight, status];
-
-	const newRow = resultsBody.insertRow(0);
-	newRow.classList.add(oddEven === "odd" ? "bg-white" : "bg-gray-50");
-
-	rowData.forEach((cellData, index) => {
-		let color = index === 0 ? "text-gray-700" : "text-gray-500";
-		color = status.toLowerCase() !== "success" ? "text-red-600" : color;
-
-		const newCell = newRow.insertCell();
-		newCell.classList.add(
-			"px-6",
-			"py-4",
-			"whitespace-nowrap",
-			"text-sm",
-			"font-medium",
-			color,
-		);
-
-		const cellDataSpan = document.createElement("span");
-		cellDataSpan.classList.add("inline-block", "max-ch");
-
-		const cellText = document.createTextNode(cellData);
-		cellDataSpan.appendChild(cellText);
-		newCell.appendChild(cellDataSpan);
-	});
-}
-
-function unhideResults() {
-	const resultsElement = document.getElementById("results");
-	resultsElement.classList.remove("hidden");
-}
+import { createProductRow, unhideResults } from "./utils";
 
 export function processBarcodeResults() {
 	const resultsBody = document.getElementById("results-body");
--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,5 +1,4 @@
 // From https://gist.github.com/calendee/b934b15d36e43782df3c2271f32801d8
-
 export function asyncWrapper<T>(somePromise: Promise<T>) {
 	return somePromise
 		.then((data) => ({ error: null, data }))
@@ -30,4 +29,41 @@ export async function fetchJson(
 
 	const json = await response?.json();
 	return { data: json, error };
+}
+
+export function createProductRow(resultsBody, productInfo, counter) {
+	const { barcode, status, info } = productInfo;
+	const { title, brand, manufacturer, weight } = info;
+	const oddEven = counter % 2 === 0 ? "odd" : "even";
+	const rowData = [barcode, title, brand, manufacturer, weight, status];
+
+	const newRow = resultsBody.insertRow(0);
+	newRow.classList.add(oddEven === "odd" ? "bg-white" : "bg-gray-50");
+
+	rowData.forEach((cellData, index) => {
+		let color = index === 0 ? "text-gray-700" : "text-gray-500";
+		color = status.toLowerCase() !== "success" ? "text-red-600" : color;
+
+		const newCell = newRow.insertCell();
+		newCell.classList.add(
+			"px-6",
+			"py-4",
+			"whitespace-nowrap",
+			"text-sm",
+			"font-medium",
+			color,
+		);
+
+		const cellDataSpan = document.createElement("span");
+		cellDataSpan.classList.add("inline-block", "max-ch");
+
+		const cellText = document.createTextNode(cellData);
+		cellDataSpan.appendChild(cellText);
+		newCell.appendChild(cellDataSpan);
+	});
+}
+
+export function unhideResults() {
+	const resultsElement = document.getElementById("results");
+	resultsElement.classList.remove("hidden");
 }
